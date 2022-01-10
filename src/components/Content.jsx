7ab6d2f2c8796780146367437f71b47cc70eb416
HuziMuzi <laif17860@mail.ru>
2022-01-10 16:56:21 +0300

    update files

---
+++ src/components/Content.jsx
@@ -0,0 +1,9 @@
+const Content = () => {
+	return (
+		<div className="content">
+			<img src="https://s1.1zoom.ru/b5050/297/Canada_Mountains_Scenery_488936_3840x2400.jpg"/>
+		</div>
+	)
+}
+
+export default Content
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+			<header className="header">
+				<img src='./imag'/>
+			</header>
+	);
+}
+
+export default Header
